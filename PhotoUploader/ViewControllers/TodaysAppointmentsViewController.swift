5b5d2d902e44f88b2e286e0cc64b1f064dce0dd5
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-11-09 15:33:58 +0100

    Filtering patients

--- PhotoUploader/ViewControllers/TodaysAppointmentsViewController.swift
+++ PhotoUploader/ViewControllers/TodaysAppointmentsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class TodaysAppointmentsViewController: UIViewController, StoryboardInitializable {
 	var patients: [User] = []
+	var filteredPatients: [User] = [] {
+		didSet {
+			tableView.reloadData()
+		}
+	}
+	
 	@IBOutlet weak var filterTextField: BorderedTextField!
 	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var navigationBar: NavigationBar!
@@ -16,6 +22,8 @@ class TodaysAppointmentsViewController: UIViewController, StoryboardInitializabl
         super.viewDidLoad()
 
 		navigationBar.delegate = self
+		filterTextField.delegate = self
+		filteredPatients = patients
     }
 
 }
@@ -32,13 +40,13 @@ extension TodaysAppointmentsViewController: UITableViewDelegate, UITableViewData
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return patients.count
+		return filteredPatients.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: PatientCell.identifier, for: indexPath) as! PatientCell
-		let patient = patients[indexPath.row]
-		cell.configure(patient, index: indexPath.row, of: patients.count)
+		let patient = filteredPatients[indexPath.row]
+		cell.configure(patient, index: indexPath.row, of: filteredPatients.count)
 		cell.didSelectCamera = {patient in
 			print("Start camera for \(patient.userName)")
 		}
@@ -47,5 +55,17 @@ extension TodaysAppointmentsViewController: UITableViewDelegate, UITableViewData
 	
 	func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
 		return UIView()
+	}
+}
+
+extension TodaysAppointmentsViewController: UITextFieldDelegate {
+	func textFieldDidChangeSelection(_ textField: UITextField) {
+		guard textField.validate != nil else {
+			self.filteredPatients = self.patients
+			return
+		}
+		self.filteredPatients = self.patients.filter {($0.userName.lowercased().contains(textField.text!.lowercased()) ||
+													   $0.token.lowercased().contains(textField.text!.lowercased()))}
+		
 	}
 }
